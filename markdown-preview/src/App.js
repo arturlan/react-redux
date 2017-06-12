830d5ede2803e8e17fe8b606856ab58ef666d0bc
Artur Lan <arturlan.us@gmail.com>
2017-06-12 18:45:04 -0400

    fixed a bug

--- markdown-preview/src/App.js
+++ markdown-preview/src/App.js
@@ -25,6 +25,7 @@ class DisplayContainer extends Component {
     this.state = {
       value:'Heading\n=======\n\nSub-heading\n-----------\n \n### Another deeper heading\n \nParagraphs are separated\nby a blank line.\n\nLeave 2 spaces at the end of a line to do a  \nline break\n\nText attributes *italic*, **bold**, \n`monospace`, ~~strikethrough~~ .\n\nShopping list:\n\n  * apples\n  * oranges\n  * pears\n\nNumbered list:\n\n  1. apples\n  2. oranges\n  3. pears\n\nThe rain---not the reign---in\nSpain.\n\n *[Herman Fassett](https://freecodecamp.com/hermanfassett)*'
     }
+    this.updateValue = this.updateValue.bind(this);
   }
 
   updateValue(modifiedValue) {
