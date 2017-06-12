5fb25126b0968fd4b17043ee73c4b99752962113
Artur Lan <arturlan.us@gmail.com>
2017-06-11 23:00:16 -0400

    markdown

--- markdown-preview/src/App.js
+++ markdown-preview/src/App.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import marked from 'marked';
+import ReactDOM from 'react-dom';
+
 
 class App extends Component {
   render() {
@@ -10,10 +13,59 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h2>Welcome to React</h2>
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <DisplayContainer />
       </div>
+    );
+  }
+}
+
+class DisplayContainer extends Component {
+  constructor() {
+    super();
+    this.state = {
+      value:'Heading\n=======\n\nSub-heading\n-----------\n \n### Another deeper heading\n \nParagraphs are separated\nby a blank line.\n\nLeave 2 spaces at the end of a line to do a  \nline break\n\nText attributes *italic*, **bold**, \n`monospace`, ~~strikethrough~~ .\n\nShopping list:\n\n  * apples\n  * oranges\n  * pears\n\nNumbered list:\n\n  1. apples\n  2. oranges\n  3. pears\n\nThe rain---not the reign---in\nSpain.\n\n *[Herman Fassett](https://freecodecamp.com/hermanfassett)*'
+    }
+  }
+
+  updateValue(modifiedValue) {
+    this.setState({
+      value: modifiedValue
+    });
+  }
+
+  rawMarkup(value) {
+    var rawMarkup = marked(value, {sanitize: true});
+    return { __html: rawMarkup};
+  }
+
+  render() {
+    return (
+      <div className="ui grid">
+        <div className="eight wide column">
+          <RawInput value={this.state.value} updateValue={this.updateValue}/>
+        </div>
+        <div className="eight wide column">
+          <span dangerouslySetInnerHTML={this.rawMarkup(this.state.value)} />
+        </div>
+      </div>
+    );
+  }
+}
+
+class RawInput extends Component {
+  constructor() {
+    super();
+    this.update = this.update.bind(this);
+  }
+
+  update() {
+    var modifiedValue=ReactDOM.findDOMNode(this.refs.inputValue).value;
+    this.props.updateValue(modifiedValue);
+  }
+
+  render() {
+    return (
+      <textarea rows="25" type="text" ref="inputValue" value={this.props.value} onChange={this.update}></textarea>
     );
   }
 }
--- markdown-preview/src/index.js
+++ markdown-preview/src/index.js
@@ -1,8 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import Checkbox from './components/Checkbox';
 import registerServiceWorker from './registerServiceWorker';
 import './index.css';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <div>
+    <App />
+    {/* <Checkbox /> */}
+  </div>, document.getElementById('root'));
 registerServiceWorker();
