ce49f1424b07c7470bb1a008fb9e03835f6157c0
Artur Lan <arturlan.us@gmail.com>
2017-06-14 14:56:27 -0400

    fixed bugs

---
+++ markdown-preview/src/components/Header.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import '../index.css';
+
+class Header extends Component {
+
+  render() {
+
+    return (
+      <div className="middle">
+        <h1 className="name">Easy README.md</h1>
+      </div>
+    )
+  }
+}
+
+export default Header;
--- markdown-preview/src/index.js
+++ markdown-preview/src/index.js
@@ -1,13 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import Checkbox from './components/Checkbox';
 import registerServiceWorker from './registerServiceWorker';
 import './index.css';
 
 ReactDOM.render(
   <div>
     <App />
-    {/* <Checkbox /> */}
   </div>, document.getElementById('root'));
 registerServiceWorker();
