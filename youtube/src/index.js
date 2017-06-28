1f991b3e13da21e2e6a0cdc2a94a4fc733774558
Artur Lan <arturlan.us@gmail.com>
2017-06-28 16:14:31 -0400

    selected videos

--- youtube/src/index.js
+++ youtube/src/index.js
@@ -9,6 +9,7 @@ import YTSearch from 'youtube-api-search';
 import reducers from './reducers';
 import SearchBar from './components/search_bar';
 import VideoList from './components/video_list';
+import VideoDetail from './components/video_detail';
 
 const API_KEY = 'AIzaSyApHR-kxXACVxc-cVMwkds09MZKV4zzeOA';
 
@@ -16,10 +17,16 @@ class App extends Component {
   constructor(props) {
     super(props)
 
-    this.state = { videos: [] };
+    this.state = {
+      videos: [],
+      selectedVideo: null
+    };
 
     YTSearch({key: API_KEY, term: 'surfboards'}, (videos) => {
-      this.setState({ videos });
+      this.setState({
+        videos: videos,
+        selectedVideo: videos[0]
+      });
     });
 
   }
@@ -28,7 +35,8 @@ class App extends Component {
     return (
       <div>
         <SearchBar />
-        <VideoList videos={this.state.videos} />
+        <VideoDetail video={this.state.selectedVideo} />
+        <VideoList onVideoSelect={selectedVideo => this.setState({selectedVideo})} videos={this.state.videos} />
       </div>
     )
   }
