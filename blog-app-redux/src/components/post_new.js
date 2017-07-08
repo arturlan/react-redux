0144ac35205cf2060e6e000346a06436fec25fcd
Artur Lan <arturlan.us@gmail.com>
2017-07-07 21:29:20 -0400

    redux-form

--- blog-app-redux/src/components/post_new.js
+++ blog-app-redux/src/components/post_new.js
@@ -2,15 +2,16 @@ import React, { Component } from 'react';
 import { Field, reduxForm } from 'redux-form';
 
 class PostsNew extends Component {
-  renderTitleField(field) {
+  renderField(field) {
     return (
       <div className="form-group">
-        <label>Title</label>
+        <label>{field.label}</label>
         <input
           className="form-control"
           type="text"
           {...field.input}
         />
+        {field.meta.error}
       </div>
     );
   }
@@ -20,15 +21,44 @@ class PostsNew extends Component {
       <div>
         <form>
           <Field
+            label="Title For Post"
             name="title"
-            component={this.renderTitleField}
+            component={this.renderField}
           />
+          <Field
+            label="Categories"
+            name="categories"
+            component={this.renderField}
+          />
+          <Field
+            label="Post Content"
+            name="content"
+            component={this.renderField}
+          />
+          <button type="submit" className="btn btn-primary">Submit</button>
         </form>
       </div>
     );
   }
 }
 
+function validate(values) {
+  const errors = {};
+
+  if (!values.title) {
+    errors.title = "Enter a title";
+  }
+  if (!values.categories) {
+    errors.categories = "Enter some categories";
+  }
+  if (!values.content) {
+    errors.content = "Enter some content";
+  }
+
+  return errors;
+}
+
 export default reduxForm({
+  validate,
   form: 'simple' // a unique identifier for this form
 })(PostsNew);
